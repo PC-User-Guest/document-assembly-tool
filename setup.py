8ca2b252be42c14afe5baff539e15e39f6cc6038
Malwandla K. Rikhotso <128429790+PC-User-Guest@users.noreply.github.com>
2026-03-19 06:29:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,12 @@ setup(
         "Intended Audience :: Legal Industry",
         "Intended Audience :: Education",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent",,
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Office/Business",
         "Topic :: Text Processing"
     ],
